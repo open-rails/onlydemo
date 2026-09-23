85e898427effc5902cf1d2830e165ebe245cf44d
Paul Fidika <paul@fidika.com>
2026-09-22 23:33:28 -0600

    Add channel creation flow to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './auth'
 import { AppLayout, NotFoundPage, RequireAccount } from './App'
 import { HomePage, ChannelsPage } from './pages/explore'
-import { ChannelPage } from './pages/channel'
+import { ChannelPage, NewChannelPage } from './pages/channel'
 import { PostPage, CheckoutReturnPage } from './pages/post'
 import { AccountPage } from './pages/account'
 import './styles.css'
@@ -14,6 +14,7 @@ const queryClient = new QueryClient({ defaultOptions: { queries: { staleTime: 20
 const router = createBrowserRouter([{ element: <AuthProvider><AppLayout /></AuthProvider>, errorElement: <NotFoundPage />, children: [
   { path: '/', element: <HomePage /> },
   { path: '/channels', element: <ChannelsPage /> },
+  { path: '/channels/new', element: <NewChannelPage /> },
   { path: '/channels/:id', element: <ChannelPage /> },
   { path: '/posts/:id', element: <PostPage /> },
   { path: '/checkout/return', element: <RequireAccount><CheckoutReturnPage /></RequireAccount> },
